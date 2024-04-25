325b1d93c4cece350b19cfae8044edc6bd814b3b
wasim475 <wasimhossain575@gmail.com>
2024-04-25 23:00:06 +0600

    route set up done

---
+++ forest-canvas/src/main.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './utility/Root';
+import ErrorPage from './Components/ErrorMsg/ErrorPage';
+import Home from './Components/Home/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children:[
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <RouterProvider router={router} />
+  </React.StrictMode>,
+)
