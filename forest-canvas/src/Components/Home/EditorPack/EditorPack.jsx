f3673b92a3f38beaf42926709ca1d4ac4ceea74f
wasim475 <wasimhossain575@gmail.com>
2024-04-30 15:09:33 +0600

    customize on editors pack

--- forest-canvas/src/Components/Home/EditorPack/EditorPack.jsx
+++ forest-canvas/src/Components/Home/EditorPack/EditorPack.jsx
@@ -11,20 +11,20 @@ const EditorPack = () => {
                     <h1 className="text-3xl font-playfair font-bold mb-3">Editors Pack</h1>
                     <p className="text-lg font-worksans text-gray-500 ">Dive into our Editor Pack section, your arsenal of creative instruments, empowering you to shape your artistic vision effortlessly.</p>
                 </div>
-                <div className="grid grid-cols-3 border-2 rounded-lg">
-                    <section data-aos="zoom-out" data-aos-duration="1000"  data-aos-delay="500" className="grid grid-cols-2 gap-2 p-4">
-                        <div className="col-span-2">
+                <div className="grid lg:grid-cols-3 md:grid-cols-3 border-2 rounded-lg">
+                    <section data-aos="zoom-out" data-aos-duration="1000"  data-aos-delay="500" className="grid md:grid-cols-2 gap-2 p-4">
+                        <div className="md:col-span-2">
                             <h1>Editors pack</h1>
                             <h1 className="text-2xl font-bold mb-3">Earth-conscious finds</h1>
                             <Link to="/allartcraftitems">
                                 <p className="flex items-center gap-x-1 font-playfair font-bold"> See more  <FaArrowRightLong/> </p>
                             </Link>
                         </div>
-                        <div className="h-40 relative max-w-xs overflow-hidden bg-cover bg-no-repeat rounded-lg">
-                            <img className="h-full w-full rounded-lg max-w-xs transition duration-300 ease-in-out hover:scale-110"  src="https://i.ibb.co/B2PYcCd/img6.jpg"/>
+                        <div className="lg:h-40 h-[100vh] w-full relative lg:max-w-xs overflow-hidden bg-cover bg-no-repeat rounded-lg">
+                            <img className="h-full w-full rounded-lg  transition duration-300 ease-in-out hover:scale-110"  src="https://i.ibb.co/B2PYcCd/img6.jpg"/>
                         </div>
-                        <div className="h-40 relative max-w-xs overflow-hidden bg-cover bg-no-repeat rounded-lg">
-                            <img className="h-full w-full rounded-lg max-w-xs transition duration-300 ease-in-out hover:scale-110"  src="https://i.ibb.co/H2wR2tb/img5.jpg"/>
+                        <div className="md:h-40 relative  overflow-hidden bg-cover bg-no-repeat rounded-lg">
+                            <img className="h-full w-full rounded-lg  transition duration-300 ease-in-out hover:scale-110"  src="https://i.ibb.co/H2wR2tb/img5.jpg"/>
                         </div>
                     </section>
                     <section className="grid grid-cols-2 gap-2" data-aos="zoom-in" data-aos-duration="1000"  data-aos-delay="1000">
@@ -42,8 +42,8 @@ const EditorPack = () => {
                         </div>
                     </section>
                     <section data-aos="fade-up-right" data-aos-duration="1000"  data-aos-delay="1500">
-                    <div className="h-80 text-center p-3 relative max-w-xs overflow-hidden bg-cover bg-no-repeat rounded-lg">
-                        <img className="h-full w-full max-w-xs transition duration-300 ease-in-out hover:scale-90" src="https://i.ibb.co/hC5rf01/The-Forest-Queen.jpg"/>
+                    <div className="lg:h-80 flex p-3 relative overflow-hidden bg-cover bg-no-repeat rounded-lg">
+                        <img className="h-full rounded-lg w-full transition duration-300 ease-in-out hover:scale-90" src="https://i.ibb.co/hC5rf01/The-Forest-Queen.jpg"/>
                     </div>
                     </section>
                 </div>
